b7f69c67fb7756fa0215f35fe071198a693a36fc
Matt Kane <matt.kane@aerian.com>
2019-06-19 13:45:14 +0100

    Publish forked package for preact X compatibility

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
-import babel from 'rollup-plugin-babel';
-import preset from 'babel-preset-es2015-minimal-rollup';
+import babel from "rollup-plugin-babel";
+import preset from "babel-preset-es2015-minimal-rollup";
 
 export default {
 	plugins: [
 		babel({
 			babelrc: false,
-			sourceMap: true,
-			exclude: 'node_modules/**',
-			presets: ['stage-0'],
+			sourceMap: false,
+			exclude: "node_modules/**",
+			presets: ["stage-0"],
 			plugins: [
-				'transform-class-properties',
-				['transform-react-jsx', { pragma:'h' }]
+				"transform-class-properties",
+				["transform-react-jsx", { pragma: "h" }]
 			].concat(preset.plugins)
 
 			// Waiting on https://github.com/rollup/rollup-plugin-babel/issues/47
